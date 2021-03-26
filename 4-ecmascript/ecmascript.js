419b98e35452ca63e9732c9f6b855caca785ec0b
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 13:13:57 -0300

    add exercicios

--- 4-ecmascript/ecmascript.js
+++ 4-ecmascript/ecmascript.js
@@ -119,4 +119,36 @@ var usuarioID = 'suportecod3r';
 var notificacoes = '9';
 var boasVindas = "Boas vindas " + usuarioID + " - " + notificacoes + " notifica\u00E7\u00F5es";
 console.log(boasVindas);
+// ==== exercicios
+// 1) Abaixo temos um código em JavaScript. "Traduza-o" para TypeScript!
+var dobro = function (valor) {
+    return valor * 2;
+};
+console.log(dobro(10));
+// 2) Verifique se há espaço para melhorias nesse trecho de código!
+var dizerOla = function (nome) {
+    if (nome === undefined) {
+        nome = "Pessoa";
+    }
+    console.log("Ola, " + nome);
+};
+dizerOla();
+dizerOla("Anna");
+// 3) Dado esse array, imprima o menor valor!
+var nums = [-3, 33, 38, 5];
+console.log('???');
+// 4) Adicione os elementos de nums em array !
+var nums = [-3, 33, 38, 5];
+var array = [55, 20];
+console.log(array);
+// 5) Simplifique os trechos de código abaixo utilizando o operador Destructuring!
+var notas = [8.5, 6.3, 9.4];
+var notas1 = notas[0];
+var notas2 = notas[1];
+var notas3 = notas[2];
+console.log(nota1, nota2, nota3);
+var cientista = { primeiroNome: "Will", experiencia: 12 };
+var primeiroNome = cientista.primeiroNome;
+var experiencia = cientista.experiencia;
+console.log(primeiroNome, experiencia);
 //# sourceMappingURL=ecmascript.js.map
