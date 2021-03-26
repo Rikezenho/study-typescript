da9da9a2a07bf5d44335494eea06cb55ed9e6852
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 13:20:51 -0300

    finish desafio 3

--- 4-ecmascript/ecmascript.js
+++ 4-ecmascript/ecmascript.js
@@ -121,34 +121,39 @@ var boasVindas = "Boas vindas " + usuarioID + " - " + notificacoes + " notifica\
 console.log(boasVindas);
 // ==== exercicios
 // 1) Abaixo temos um código em JavaScript. "Traduza-o" para TypeScript!
-var dobro = function (valor) {
-    return valor * 2;
-};
-console.log(dobro(10));
+{
+    var dobro = function (valor) {
+        return valor * 2;
+    };
+    console.log(dobro(10));
+}
 // 2) Verifique se há espaço para melhorias nesse trecho de código!
-var dizerOla = function (nome) {
-    if (nome === undefined) {
-        nome = "Pessoa";
-    }
-    console.log("Ola, " + nome);
-};
-dizerOla();
-dizerOla("Anna");
+{
+    var dizerOla = function (nome) {
+        if (nome === void 0) { nome = 'Pessoa'; }
+        console.log("Ola, " + nome);
+    };
+    dizerOla();
+    dizerOla("Anna");
+}
 // 3) Dado esse array, imprima o menor valor!
-var nums = [-3, 33, 38, 5];
-console.log('???');
+{
+    var nums = [-3, 33, 38, 5];
+    console.log(Math.min.apply(Math, nums));
+}
 // 4) Adicione os elementos de nums em array !
-var nums = [-3, 33, 38, 5];
-var array = [55, 20];
-console.log(array);
+{
+    var nums = [-3, 33, 38, 5];
+    var array = __spreadArray([55, 20], nums);
+    console.log(array);
+}
 // 5) Simplifique os trechos de código abaixo utilizando o operador Destructuring!
-var notas = [8.5, 6.3, 9.4];
-var notas1 = notas[0];
-var notas2 = notas[1];
-var notas3 = notas[2];
-console.log(nota1, nota2, nota3);
-var cientista = { primeiroNome: "Will", experiencia: 12 };
-var primeiroNome = cientista.primeiroNome;
-var experiencia = cientista.experiencia;
-console.log(primeiroNome, experiencia);
+{
+    var _a = [8.5, 6.3, 9.4], nota1 = _a[0], nota2 = _a[1], nota3 = _a[2];
+    console.log(nota1, nota2, nota3);
+}
+{
+    var _b = { primeiroNome: "Will", experiencia: 12 }, primeiroNome = _b.primeiroNome, experiencia = _b.experiencia;
+    console.log(primeiroNome, experiencia);
+}
 //# sourceMappingURL=ecmascript.js.map
--- 4-ecmascript/ecmascript.ts
+++ 4-ecmascript/ecmascript.ts
@@ -137,36 +137,42 @@ console.log(boasVindas)
 // ==== exercicios
 
 // 1) Abaixo temos um código em JavaScript. "Traduza-o" para TypeScript!
-var dobro = function(valor) {
-return valor * 2
+{
+    const dobro: (valor: number) => number = (valor: number): number => {
+        return valor * 2
+    }
+    console.log(dobro(10))
 }
-console.log(dobro(10))
 
 // 2) Verifique se há espaço para melhorias nesse trecho de código!
-var dizerOla = function (nome) {
-if (nome === undefined) { nome = "Pessoa" }
-console.log("Ola, " + nome)
+{
+    const dizerOla = (nome: null | string = 'Pessoa'): void => {
+        console.log(`Ola, ${nome}`)
+    }
+    dizerOla()
+    dizerOla("Anna")
 }
-dizerOla()
-dizerOla("Anna")
 
 // 3) Dado esse array, imprima o menor valor!
-var nums = [-3, 33, 38, 5]
-console.log('???')
+{
+    const nums: number[] = [-3, 33, 38, 5]
+    console.log(Math.min(...nums))
+}
 
 // 4) Adicione os elementos de nums em array !
-var nums = [-3, 33, 38, 5]
-var array = [55, 20]
-console.log(array)
+{
+    const nums: number[] = [-3, 33, 38, 5]
+    const array: number[] = [55, 20, ...nums]
+    console.log(array)
+}
 
 // 5) Simplifique os trechos de código abaixo utilizando o operador Destructuring!
-var notas = [8.5, 6.3, 9.4]
-var notas1 = notas[0]
-var notas2 = notas[1]
-var notas3 = notas[2]
-console.log(nota1, nota2, nota3)
+{
+    const [nota1, nota2, nota3] = [8.5, 6.3, 9.4]
+    console.log(nota1, nota2, nota3)
+}
 
-var cientista = {primeiroNome: "Will", experiencia: 12}
-var primeiroNome = cientista.primeiroNome
-var experiencia = cientista.experiencia
-console.log(primeiroNome, experiencia)
+{
+    const { primeiroNome, experiencia } = { primeiroNome: "Will", experiencia: 12 }
+    console.log(primeiroNome, experiencia)
+}
