690529b2b1e638db98750106f60fd18fb99fcdf2
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 03:12:14 -0300

    desafio 2

--- 2-tipos/tipos.js
+++ 2-tipos/tipos.js
@@ -141,3 +141,16 @@ podeSerNulo = 12;
 console.log(podeSerNulo);
 podeSerNulo = 'abc';
 console.log(podeSerNulo);
+var contaBancaria = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+var correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
--- 2-tipos/tipos.ts
+++ 2-tipos/tipos.ts
@@ -188,3 +188,35 @@ podeSerNulo = 12
 console.log(podeSerNulo)
 podeSerNulo = 'abc'
 console.log(podeSerNulo)
+
+/**
+ * ==== Desafio
+ * Colocar o maximo de tipos possiveis no codigo abaixo
+ */
+
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+
+type Correntista = {
+    nome: string,
+    contaBancaria: ContaBancaria,
+    contatos: string[]
+}
+
+let contaBancaria: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+}
+ 
+let correntista: Correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+ 
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
