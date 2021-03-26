0b783a3157b8fa69da6388d00c26c79ef19dd187
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 17:16:31 -0300

    first commit of classes

---
+++ 5-classes/classes.ts
@@ -0,0 +1,27 @@
+class Data {
+    // publico por padrão
+    dia: number
+    public mes: number
+    ano: number 
+
+    constructor(
+        dia: number = 1,
+        mes: number = 1,
+        ano: number = 1970
+    ) {
+        this.dia = dia
+        this.mes = mes
+        this.ano = ano
+    }
+
+
+}
+
+const aniversario = new Data(3, 11, 1991)
+aniversario.dia = 4
+console.log(aniversario.dia)
+console.log(aniversario)
+
+const casamento = new Data;
+casamento.ano = 2017
+console.log(casamento)
