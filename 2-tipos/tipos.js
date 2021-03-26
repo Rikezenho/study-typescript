d508036f682b5a47929ddecf6d9d43e0edd7c6dd
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 01:47:43 -0300

    inferência de tipos

--- 2-tipos/tipos.js
+++ 2-tipos/tipos.js
@@ -1,1 +1,15 @@
 "use strict";
+// string
+var nome = 'João';
+console.log(nome);
+// nome = 28
+// numbers
+var idade = 27;
+// idade = 'Ana'
+idade = 27.5;
+console.log(idade);
+// boolean
+var possuiHobbies = false;
+possuiHobbies = true;
+// possuiHobbies = 1
+console.log(possuiHobbies);
---
+++ 2-tipos/tipos.ts
@@ -0,0 +1,16 @@
+// string
+let nome = 'João'
+console.log(nome)
+// nome = 28
+
+// numbers
+let idade = 27
+// idade = 'Ana'
+idade = 27.5
+console.log(idade)
+
+// boolean
+let possuiHobbies = false
+possuiHobbies = true
+// possuiHobbies = 1
+console.log(possuiHobbies)
