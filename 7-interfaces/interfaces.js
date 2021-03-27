d7b51727834477c9bf54971d9aa13818e1c0f7b8
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 02:16:04 -0300

    estender Object com interface

--- 7-interfaces/interfaces.js
+++ 7-interfaces/interfaces.js
@@ -55,4 +55,18 @@ class AbstrataABD {
     a() { }
     b() { }
 }
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 2;
+const y = 3;
+const z = 4;
+console.log(x);
+console.log(y);
+console.log(z);
+x.log();
+y.log();
+z.log();
+const cli = { nome: 'Pedro', toString() { return this.nome; } };
+cli.log();
 //# sourceMappingURL=interfaces.js.map
--- 7-interfaces/interfaces.ts
+++ 7-interfaces/interfaces.ts
@@ -93,3 +93,25 @@ abstract class AbstrataABD implements A, B {
     abstract d(): void
 }
 
+interface Object {
+    log(): void
+}
+
+Object.prototype.log = function() {
+    console.log(this.toString())
+}
+
+const x = 2
+const y = 3
+const z = 4
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+x.log()
+y.log()
+z.log()
+
+const cli = { nome: 'Pedro', toString() { return this.nome } }
+cli.log()
