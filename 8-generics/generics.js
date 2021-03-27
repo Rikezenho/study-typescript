7974aa2a99602681879c52f7eed7d1c0f3149bbb
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 02:36:43 -0300

    generics

--- 8-generics/generics.js
+++ 8-generics/generics.js
@@ -1,2 +1,15 @@
 "use strict";
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo('João').length);
+console.log(echo(27).length);
+console.log(echo({ nome: 'João', idade: 27 }));
+// ==== usando generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('João').length);
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
 //# sourceMappingURL=generics.js.map
---
+++ 8-generics/generics.ts
@@ -0,0 +1,16 @@
+function echo(objeto: any) {
+    return objeto
+}
+
+console.log(echo('João').length)
+console.log(echo(27).length)
+console.log(echo({ nome: 'João', idade: 27 }))
+
+// ==== usando generics
+function echoMelhorado<T>(objeto: T): T {
+    return objeto
+}
+
+console.log(echoMelhorado('João').length)
+console.log(echoMelhorado<number>(27))
+console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome)
