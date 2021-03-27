14922486aa58dc9888b4fc64800f91eaa1b93ebf
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 01:33:15 -0300

    systemjs

--- 6-modulos/modulos.js
+++ 6-modulos/modulos.js
@@ -1,2 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const retangulo_1 = __importDefault(require("./retangulo"));
+// import { areaCircunferencia } from './circunferencia'
+const circunferencia_1 = require("./circunferencia");
+console.log('Módulo carregado...');
+console.log(retangulo_1.default(7, 8));
+console.log(circunferencia_1.areaCircunferencia(2));
 //# sourceMappingURL=modulos.js.map
---
+++ 6-modulos/modulos.ts
@@ -0,0 +1,7 @@
+import retangulo from './retangulo'
+// import { areaCircunferencia } from './circunferencia'
+import { areaCircunferencia as circ } from './circunferencia'
+
+console.log('Módulo carregado...')
+console.log(retangulo(7,8))
+console.log(circ(2))
