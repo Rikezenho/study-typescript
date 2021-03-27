fb0d6c32ecbc3e870fa566078d9f7781f2f8596f
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 02:50:15 -0300

    usando os generics para 'especificar'

--- 8-generics/generics.js
+++ 8-generics/generics.js
@@ -12,4 +12,26 @@ function echoMelhorado(objeto) {
 console.log(echoMelhorado('João').length);
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome);
+// ==== generics disponíveis na API
+const avaliacoes = [10, 9.3, 7.7];
+avaliacoes.push(8.4);
+// avaliacoes.push('5.5')
+console.log(avaliacoes);
+// ==== Array
+function imprimir(args) {
+    args.forEach(elemento => console.log(elemento));
+}
+imprimir([1, 2, 3]);
+imprimir([1, 2, 3]);
+imprimir(['a', 'b', 'c']);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 },
+]);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 },
+]);
 //# sourceMappingURL=generics.js.map
--- 8-generics/generics.ts
+++ 8-generics/generics.ts
@@ -14,3 +14,30 @@ function echoMelhorado<T>(objeto: T): T {
 console.log(echoMelhorado('João').length)
 console.log(echoMelhorado<number>(27))
 console.log(echoMelhorado({ nome: 'João', idade: 27 }).nome)
+
+
+// ==== generics disponíveis na API
+const avaliacoes: Array<number> = [10, 9.3, 7.7]
+avaliacoes.push(8.4)
+// avaliacoes.push('5.5')
+console.log(avaliacoes)
+
+// ==== Array
+function imprimir<T>(args: T[]) {
+    args.forEach(elemento => console.log(elemento))
+}
+
+imprimir([1, 2, 3])
+imprimir<number>([1, 2, 3])
+imprimir<string>(['a', 'b', 'c'])
+imprimir<{ nome: string, idade: number }>([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 },
+])
+type Aluno = { nome: string, idade: number }
+imprimir<Aluno>([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 },
+])
