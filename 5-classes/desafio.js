9919e100a8f39e612db85fc21d0493d555b93eb1
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-26 23:50:08 -0300

    desafio 5

--- 5-classes/desafio.js
+++ 5-classes/desafio.js
@@ -1,51 +1,54 @@
 "use strict";
 // Exercício 1 - Classe
-function Moto(nome) {
-    this.nome = nome;
-    this.velocidade = 0;
-    this.buzinar = function () {
+class Moto {
+    constructor(nome) {
+        this.nome = nome;
+        this.velocidade = 0;
+    }
+    buzinar() {
         console.log('Toooooooooot!');
-    };
-    this.acelerar = function (delta) {
+    }
+    acelerar(delta) {
         this.velocidade = this.velocidade + delta;
-    };
+    }
 }
-var moto = new Moto('Ducati');
+const moto = new Moto('Ducati');
 moto.buzinar();
 console.log(moto.velocidade);
 moto.acelerar(30);
 console.log(moto.velocidade);
 // Exercício 2 - Herança
-var objeto2D = {
-    base: 0,
-    altura: 0
-};
-var retangulo = Object.create(objeto2D);
-retangulo.base = 5;
-retangulo.altura = 7;
-retangulo.area = function () {
-    return this.base * this.altura;
-};
+class Objeto2D {
+    constructor(base = 0, altura = 0) {
+        this.base = base;
+        this.altura = altura;
+    }
+}
+class Retangulo extends Objeto2D {
+    area() {
+        return this.base * this.altura;
+    }
+}
+const retangulo = new Retangulo(5, 7);
 console.log(retangulo.area());
 // Exercício 3 - Getters & Setters
-var estagiario = {
-    _primeiroNome: ''
-};
-Object.defineProperty(estagiario, 'primeiroNome', {
-    get: function () {
+class Estagiario {
+    constructor(_primeiroNome = '') {
+        this._primeiroNome = _primeiroNome;
+    }
+    get primeiroNome() {
         return this._primeiroNome;
-    },
-    set: function (valor) {
+    }
+    set primeiroNome(valor) {
         if (valor.length >= 3) {
             this._primeiroNome = valor;
         }
         else {
             this._primeiroNome = '';
         }
-    },
-    enumerable: true,
-    configurable: true
-});
+    }
+}
+const estagiario = new Estagiario;
 console.log(estagiario.primeiroNome);
 estagiario.primeiroNome = 'Le';
 console.log(estagiario.primeiroNome);
--- 5-classes/desafio.ts
+++ 5-classes/desafio.ts
@@ -1,57 +1,62 @@
 // Exercício 1 - Classe
-function Moto(nome) {
-    this.nome = nome
-    this.velocidade = 0
- 
-    this.buzinar = function() {
+class Moto {
+    public velocidade: number = 0
+
+    constructor(
+        public nome: string,
+    ) {}
+
+    public buzinar(): void {
         console.log('Toooooooooot!')
     }
- 
-    this.acelerar= function(delta) {
+
+    public acelerar(delta: number): void {
         this.velocidade = this.velocidade + delta
     }
 }
  
-var moto = new Moto('Ducati')
+const moto: Moto = new Moto('Ducati')
 moto.buzinar()
 console.log(moto.velocidade)
 moto.acelerar(30)
 console.log(moto.velocidade)
  
 // Exercício 2 - Herança
-var objeto2D = {
-    base: 0,
-    altura: 0
+abstract class Objeto2D {
+    constructor(
+        public base: number = 0,
+        public altura: number = 0
+    ) {}
+
+    abstract area(): number
+}
+
+class Retangulo extends Objeto2D {
+    area(): number {
+        return this.base * this.altura
+    }
 }
  
-var retangulo = Object.create(objeto2D)
-retangulo.base = 5
-retangulo.altura = 7
-retangulo.area = function() {
-    return this.base * this.altura
-}
+const retangulo = new Retangulo(5, 7)
 console.log(retangulo.area())
  
 // Exercício 3 - Getters & Setters
-var estagiario = {
-    _primeiroNome: ''
-}
- 
-Object.defineProperty(estagiario, 'primeiroNome', {
-    get: function () {
+class Estagiario {
+    constructor(private _primeiroNome: string = '') { }
+
+    get primeiroNome(): string {
         return this._primeiroNome
-    },
-    set: function (valor) {
+    }
+    set primeiroNome(valor: string) {
         if (valor.length >= 3) {
             this._primeiroNome = valor
         } else {
             this._primeiroNome = ''
         }
-    },
-    enumerable: true,
-    configurable: true
-})
- 
+    }
+}
+
+const estagiario = new Estagiario;
 console.log(estagiario.primeiroNome)
 estagiario.primeiroNome = 'Le'
 console.log(estagiario.primeiroNome)
