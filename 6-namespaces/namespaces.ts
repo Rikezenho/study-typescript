cde6270b6b3ea408453c5df4f0f8901f5eb3f007
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 01:10:57 -0300

    namespaces

--- 6-namespaces/namespaces.ts
+++ 6-namespaces/namespaces.ts
@@ -1,18 +1,9 @@
-namespace Areas {
-    const PI = 3.14
-    
-    export function circunferencia(raio: number) {
-        return PI * Math.pow(raio, 2)
-    }
-    
-    export function retangulo(base: number, altura: number): number {
-        return base * altura
-    }
-}
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 
 const PI = 2.99
 
-console.log(Areas.circunferencia(10))
-console.log(Areas.retangulo(10, 20))
+console.log(Geometria.Area.circunferencia(10))
+console.log(Geometria.Area.retangulo(10, 20))
 
 console.log(PI)
