bcfbd499acea400bb63a3604cfddd19759935354
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-27 01:23:40 -0300

    add modulos

---
+++ 6-modulos/retangulo.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=retangulo.js.map
--- 6-namespaces/namespaces.ts
+++ 6-namespaces/namespaces.ts
@@ -1,9 +1,9 @@
 ///<reference path="geometriaCirc.ts"/>
 ///<reference path="geometriaRect.ts"/>
 
-const PI = 2.99
+// const PI = 2.99
 
 console.log(Geometria.Area.circunferencia(10))
 console.log(Geometria.Area.retangulo(10, 20))
 
-console.log(PI)
+// console.log(PI)
