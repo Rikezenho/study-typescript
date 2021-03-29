fe2f226f1f08f71a57d64d217288d13490e3eb8b
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-29 04:50:35 -0300

    computed property

--- integracao-vue/src/componentes/ContadorClasse.vue
+++ integracao-vue/src/componentes/ContadorClasse.vue
@@ -2,6 +2,8 @@
     <div>
         <h1>Contador (Classe)</h1>
         <h2>{{ valor }}</h2>
+        <h3>{{ getParOuImpar() }}</h3>
+        <h3>{{ parOuImpar }}</h3>
         <button @click="setValor(-1)">Dec</button>
         <button @click="setValor(1)">Inc</button>
     </div>
@@ -17,6 +19,12 @@ export default class ContadorClasse extends Vue {
 
     public setValor(delta: number) {
         this.valor += delta;
+    }
+    public getParOuImpar(): string {
+        return this.valor % 2 === 0 ? 'Par' : 'Impar';
+    }
+    public get parOuImpar(): string {
+        return this.valor % 2 === 0 ? 'Par' : 'Impar';
     }
 }
 </script>
