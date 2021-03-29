830c3b63fc05ef92bc160e0896e76bc1e157ffee
Henrique Kuwai <henrique.kuwai@infracommerce.com.br>
2021-03-29 04:41:00 -0300

    integração com vue - component comum, component baseado em classe

---
+++ integracao-vue/src/componentes/ContadorClasse.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Contador (Classe)</h1>
+        <h2>{{ valor }}</h2>
+        <button @click="setValor(-1)">Dec</button>
+        <button @click="setValor(1)">Inc</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class ContadorClasse extends Vue {
+    private valor: number = 0;
+
+    public setValor(delta: number) {
+        this.valor += delta;
+    }
+}
+</script>
+
+<style>
+
+</style>
